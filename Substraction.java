6ce77b24bd6a212d8e1e86f7bf8c81f36600c2de
sharmavs2001 <sharmavs2001@gmail.com>
2021-05-29 19:57:25 +0530

    Branch1 commit

--- Substraction.java
+++ Substraction.java
@@ -10,11 +10,11 @@ public class Substraction {
 
 		
 		 Scanner sc= new Scanner(System.in);
-		 System.out.println("Substraction - Enter 2 numbers!");
+		 System.out.println("Addition - Enter 2 numbers!");
 		 int a= sc.nextInt();
 		 int b= sc.nextInt();
 
-		 System.out.println( " Substraction = " + ( a - b ) );
+		 System.out.println( " Addition = " + ( a + b ) );
 		 
 		
 
